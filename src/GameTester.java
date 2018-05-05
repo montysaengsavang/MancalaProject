1983a2f9781a0368d451df502febdec028517e09
montysaengsavang <33338042+montysaengsavang@users.noreply.github.com>
2018-05-05 11:28:46 -0700

    erase main code
    erase main code

--- src/GameTester.java
+++ src/GameTester.java
@@ -8,32 +8,6 @@ public class GameTester {
 		
 	    BoardFrame gameFrame = new BoardFrame();
 	    
-	/*	
-		Scanner in = new Scanner(System.in);
-		System.out.println("How many initial stones?");
-		int initial = in.nextInt();
-		Board b = new Board(initial);
-		System.out.println();
-		System.out.println("Turn " + b.getTurn());
-		b.display();
-		boolean end = false;
-		in = new Scanner(System.in);
-		while (!end) {
-			String inp = in.nextLine();
-			if (inp.equals("undo"))
-				b.undo();
-			else
-				b.select(inp.toUpperCase());
-			System.out.println("Turn " + b.getTurn());
-			b.display();
-			end = b.checkEnd();
-		}
-		b.calculateTotal();
-		System.out.println("--------------------------------------------------");
-		b.display();
-		System.out.println(b.getWinner());
-		in.close();
-*/
 	}
 
 }
