f52eeb92fbb57016b204d27f055f0c140df42976
H4RT4N <38451084+H4RT4N@users.noreply.github.com>
2018-05-05 21:57:33 -0700

    Add files via upload

---
+++ src/MancalaTester.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class MancalaTester {
+
+	
+	/**
+	 * This is the main class, that creates a boardframe object
+	 * @author Tran, Harry; Mansahia, Shahbaz Singh; Saengsavang, Monty;
+	 *
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+	    BoardFrame gameFrame = new BoardFrame();
+	    
+	}
+
+}
