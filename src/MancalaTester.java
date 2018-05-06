4b7a6dcc135150f34f126c1610a233485670890f
H4RT4N <38451084+H4RT4N@users.noreply.github.com>
2018-05-05 21:58:42 -0700

    Update MancalaTester.java

--- src/MancalaTester.java
+++ src/MancalaTester.java
@@ -4,7 +4,6 @@ public class MancalaTester {
 	/**
 	 * This is the main class, that creates a boardframe object
 	 * @author Tran, Harry; Mansahia, Shahbaz Singh; Saengsavang, Monty;
-	 *
 	 */
 	public static void main(String[] args) {
 	    BoardFrame gameFrame = new BoardFrame();    
