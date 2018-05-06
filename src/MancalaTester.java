dbcff9c43a7a020857abac1b7a46fe4d58926766
H4RT4N <38451084+H4RT4N@users.noreply.github.com>
2018-05-05 21:58:20 -0700

    Update MancalaTester.java

--- src/MancalaTester.java
+++ src/MancalaTester.java
@@ -1,19 +1,12 @@
 import java.util.Scanner;
 
 public class MancalaTester {
-
-	
 	/**
 	 * This is the main class, that creates a boardframe object
 	 * @author Tran, Harry; Mansahia, Shahbaz Singh; Saengsavang, Monty;
 	 *
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-		
-	    BoardFrame gameFrame = new BoardFrame();
-	    
+	    BoardFrame gameFrame = new BoardFrame();    
 	}
-
 }
